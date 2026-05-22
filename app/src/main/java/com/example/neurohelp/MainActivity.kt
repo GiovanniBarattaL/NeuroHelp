b7d32cad348d7436a766a3983b90d81f93101717
GiovanniBarattaL <giovannibaratta.biz@gmail.com>
2026-05-22 07:34:01 -0300

    Initial commit

---
+++ app/src/main/java/com/example/neurohelp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.neurohelp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnprofessional = findViewById<Button>(R.id.btnProfissional)
+        btnprofessional.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+            overridePendingTransition(0, 0)
+        }
+
+    }
+}
