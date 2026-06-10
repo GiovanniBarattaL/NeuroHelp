e66aaf81d701bbf65809dfdc4798eea675dff8c3
GiovanniBarattaL <giovannibaratta.biz@gmail.com>
2026-06-10 08:26:22 -0300

    09/06

--- app/src/main/java/com/example/neurohelp/MainActivity7.kt
+++ app/src/main/java/com/example/neurohelp/MainActivity7.kt
@@ -3,6 +3,7 @@ package com.example.neurohelp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,10 +19,22 @@ class MainActivity7 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val pbtnprox= findViewById<Button>(R.id.pbtnprox)
+        pbtnprox.setOnClickListener {
+            val intent = Intent(this, MainActivity8::class.java)
+            startActivity(intent)
+        }
+
+        val txtlogin4= findViewById<TextView>(R.id.fazerlogin4)
+        txtlogin4.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
         val btnvoltar5= findViewById<Button>(R.id.btnvoltar5)
         btnvoltar5.setOnClickListener {
             val intent = Intent(this, MainActivity3::class.java)
             startActivity(intent)
         }
     }
-}
+}}
--- app/src/main/java/com/example/neurohelp/MainActivity8.kt
+++ app/src/main/java/com/example/neurohelp/MainActivity8.kt
@@ -1,6 +1,12 @@
 package com.example.neurohelp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +22,39 @@ class MainActivity8 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val txtlogin5= findViewById<TextView>(R.id.fazerlogin5)
+        txtlogin5.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        val btnvoltar6= findViewById<Button>(R.id.btnvoltar6)
+        btnvoltar6.setOnClickListener {
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
+
+        val upload = findViewById<LinearLayout>(R.id.layoutUpload)
+
+        upload.setOnClickListener {
+
+        }
+        val profissao = findViewById<AutoCompleteTextView>(R.id.spProfissao)
+
+        val itens = arrayOf(
+            "Psicólogo",
+            "Fonoaudiólogo",
+            "Terapeuta Ocupacional",
+            "Pedagogo",
+            "Neuropediatra"
+        )
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_dropdown_item_1line,
+            itens
+        )
+
+        profissao.setAdapter(adapter)
     }
-}
+}}
