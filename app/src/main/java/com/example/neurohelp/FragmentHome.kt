cd4054fd8c5d4fe624ae9f2dd71274cd9229c06e
Diego dos Santos <diegodsguinazu@gmail.com>
2026-08-26 12:37:54 -0300

    Perfil 26/08

--- app/src/main/java/com/example/neurohelp/FragmentHome.kt
+++ app/src/main/java/com/example/neurohelp/FragmentHome.kt
@@ -1,9 +1,11 @@
 package com.example.neurohelp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 
 class FragmentHome : Fragment() {
@@ -18,5 +20,18 @@ class FragmentHome : Fragment() {
             container,
             false
         )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imgPerfil = view.findViewById<ImageView>(R.id.imgPerfil)
+
+        imgPerfil.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, FragmentPerfil())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
